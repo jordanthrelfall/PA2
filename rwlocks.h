887fd15bbb2c36b91335439c679db274536ca19a
maximusWhitaker <122586215+maximusWhitaker@users.noreply.github.com>
2024-04-07 20:03:42 -0400

    Add files via upload

---
+++ rwlocks.h
@@ -0,0 +1,34 @@
+#ifndef __rwlocks_h__
+#define __rwlocks_h__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "common_threads.h"
+
+typedef struct _rwlock_t {
+    sem_t writelock;
+    sem_t lock;
+    int readers;
+} rwlock_t;
+
+void rwlock_init(rwlock_t *lock); //initialze values in new lock
+
+void rwlock_acquire_readlock(rwlock_t *lock); //acquire the read lock
+
+void rwlock_release_readlock(rwlock_t *lock); //release the write lock
+
+void rwlock_acquire_writelock(rwlock_t *lock); //acquire the read lock
+
+void rwlock_release_writelock(rwlock_t *lock); //release the write lock
+
+rwlock_t mutex;
+
+void *reader(void *arg);
+
+void *writer(void *arg);
+
+#endif // __rwlocks_h__
