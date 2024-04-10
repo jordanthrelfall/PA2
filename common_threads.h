f13e6ad370cedcd972c9e8faf8bbce396ba85042
maximusWhitaker <122586215+maximusWhitaker@users.noreply.github.com>
2024-04-10 18:46:53 -0400

    Update common_threads.h

--- common_threads.h
+++ common_threads.h
@@ -7,10 +7,6 @@
 #include <assert.h>
 #include <sched.h>
 
-#ifdef __linux__
-#include <semaphore.h>
-#endif
-
 #define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
 #define Pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
 
